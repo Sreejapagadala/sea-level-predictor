0a4c706f69a53d8e66497e542643ec3e869be168
Sreeja Pagadala <122866553+Sreejapagadala@users.noreply.github.com>
2026-05-08 02:44:38 +0530

    Create unit test for draw_plot function
    Add unit test for draw_plot function in sea_level_predictor.

---
+++ test_module.py
@@ -0,0 +1,13 @@
+import unittest
+from sea_level_predictor import draw_plot
+
+
+class SeaLevelPredictorTest(unittest.TestCase):
+
+    def test_draw_plot(self):
+        fig = draw_plot()
+        self.assertIsNotNone(fig)
+
+
+if __name__ == "__main__":
+    unittest.main()
